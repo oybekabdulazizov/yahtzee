66c042a747a079c27afb49710c7b918a2a829976
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-01-09 14:23:01 +0100

    add Die, Dice components, and Die stylesheet

---
+++ src/Die.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+  render() {
+    return <button className='Die'>Die</button>;
+  }
+}
+
+export default Die;
