854f8c9bf6294c1af2475ec4c994c2b60c1b8996
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 16:34:49 +0200

    add styles to the RuleRow component

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './RuleRow.css';
+
 export default function RuleRow({ name, description, score, doScore }) {
   const disabled = score !== undefined;
+  let classes = `RuleRow-row ${disabled ? `disabled` : ``}`;
   return (
-    <tr onClick={disabled ? null : doScore}>
+    <tr onClick={disabled ? null : doScore} className={classes}>
       <td>{name}</td>
       <td>{disabled ? score : description}</td>
     </tr>
