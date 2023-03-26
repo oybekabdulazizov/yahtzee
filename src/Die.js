435b93aafb4fd13dbf62943b3b15e221d6ec337e
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 11:43:33 +0200

    replace dice values with dice icons

--- src/Die.js
+++ src/Die.js
@@ -1,15 +1,36 @@
 import React, { useContext } from 'react';
 import { FunctionsContext } from './contexts/game.context';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSquare,
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './Die.css';
 
 export default function Die({ id, locked, val }) {
   const { toggleDie } = useContext(FunctionsContext);
 
+  const dices = {
+    0: faSquare,
+    1: faDiceOne,
+    2: faDiceTwo,
+    3: faDiceThree,
+    4: faDiceFour,
+    5: faDiceFive,
+    6: faDiceSix,
+  };
+
   let classDie = 'Die ' + (locked ? 'Die-locked' : '');
+  console.log(dices[val]);
   return (
     <button onClick={() => toggleDie(id)} className={classDie}>
-      {val}
+      <FontAwesomeIcon icon={dices[val]} size='2xl' />
     </button>
   );
 }
