84af5805350fedc879ffb0adc4ac7fccf2288cd0
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 11:52:51 +0200

    move the Dice code into HeaderDice component and remove the Dice component

--- src/HeaderDice.js
+++ src/HeaderDice.js
@@ -1,15 +1,21 @@
 import React, { useContext } from 'react';
 
-import Dice from './Dice';
+import Die from './Die';
 import { FunctionsContext, StateContext } from './contexts/game.context';
 
 export default function HeaderDice() {
   const state = useContext(StateContext);
   const { roll } = useContext(FunctionsContext);
 
+  const renderedDice = () => {
+    return state.dice.map((dieVal, id) => (
+      <Die val={dieVal} id={id} key={id} locked={state.locked[id]} />
+    ));
+  };
+
   return (
     <section>
-      <Dice />
+      <div>{renderedDice()}</div>
       <div>
         <button onClick={roll} disabled={state.locked.every((i) => i === true)}>
           {state.rollsLeft} Rerolls left
