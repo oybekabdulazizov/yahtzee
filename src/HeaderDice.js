9455f17d225ae75243af28537264593f66d25022
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 16:23:00 +0200

    style up the HeaderDice component

--- src/HeaderDice.js
+++ src/HeaderDice.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 
+import './HeaderDice.css';
+
 import Die from './Die';
 import { FunctionsContext, StateContext } from './contexts/game.context';
 
@@ -9,16 +11,27 @@ export default function HeaderDice() {
 
   const renderedDice = () => {
     return state.dice.map((dieVal, id) => (
-      <Die val={dieVal} id={id} key={id} locked={state.locked[id]} />
+      <Die
+        val={dieVal}
+        id={id}
+        key={id}
+        locked={state.locked[id]}
+        disabled={state.rollsLeft < 1}
+      />
     ));
   };
 
   return (
-    <section>
-      <div>{renderedDice()}</div>
+    <section className='HeaderDice'>
+      <h1 className='HeaderDice-title'>Yahtzee!</h1>
+      <div className='HeaderDice-dice'>{renderedDice()}</div>
       <div>
-        <button onClick={roll} disabled={state.locked.every((i) => i === true)}>
-          {state.rollsLeft} Rerolls left
+        <button
+          onClick={roll}
+          className='HeaderDice-rollbtn'
+          disabled={state.locked.every((i) => i === true)}
+        >
+          {state.rollsLeft} Rerolls Left
         </button>
       </div>
     </section>
