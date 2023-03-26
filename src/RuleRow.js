2ec2e9ceb4ba2d108449e3588bb27f57a9926007
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 10:56:47 +0200

    disable the ability to re-use a score

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function RuleRow({ name, description, score, doScore }) {
   return (
-    <tr onClick={doScore}>
+    <tr onClick={score === undefined ? doScore : null}>
       <td>{name}</td>
       <td>{description}</td>
       <td>{score}</td>
