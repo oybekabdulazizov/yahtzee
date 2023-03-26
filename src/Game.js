b24f476dbf1d86ef3534d88108e407d94d61bf65
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-03-26 16:21:48 +0200

    style up the Game component

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { GameProvider } from './contexts/game.context';
 
+import './Game.css';
+
 import HeaderDice from './HeaderDice';
 import ScoreTable from './ScoreTable';
 
 export default function Game() {
   return (
-    <GameProvider>
-      <HeaderDice />
-      <ScoreTable />
-    </GameProvider>
+    <div className='Game'>
+      <GameProvider>
+        <HeaderDice />
+        <ScoreTable />
+      </GameProvider>
+    </div>
   );
 }
